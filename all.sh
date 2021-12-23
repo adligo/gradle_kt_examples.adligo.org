87e3c9967ec8a778f5de48c08c29d0c612859f53
Scott Morgan <scott@adligo.com>
2021-12-22 18:01:20 -0600

    Fixing the test script.

--- all.sh
+++ all.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #Expand new lines \n
 shopt -s xpg_echo
-
+tdir=`pwd`
 function exec() {
+  cd $tdir
+  #pwd
   dir=$1
   cd $dir
   status=$?
@@ -12,9 +14,11 @@ function exec() {
   if [[ "$status" == "0" ]]; then
     echo "$cmd exited with success in $dir"
   else
-    echo "$cmd exited with FAILURE in $dir"
+    echo "$cmd exited with FAILURE $status in $dir"
     exit 9
   fi
 }
 
 exec "basic/first" "gradle build > /dev/null 2>&1"
+exec "basic/printOutline" "gradle hw > /dev/null 2>&1"
+exec "basic/printMultiProjectOutline" "gradle hw > /dev/null 2>&1"
